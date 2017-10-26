2ab9a6cfe71cdc24e3010d98650ed28d1f84b8f0
Dem <dstaranenko@gmail.com>
2017-10-26 16:47:58 +0300

    up

--- resources/views/layouts/contacts.blade.php
+++ resources/views/layouts/contacts.blade.php
@@ -7,7 +7,7 @@
 <img class="topLine" src="/svg/top.svg">
 
 <style type="text/css">
-	article {
+	.contacts {
 		margin: 40px 0px;
 	}
 </style>
@@ -16,25 +16,25 @@
     <div class="container">
 					
 			<div class="row">
-				<article class="col-sm-6">
+				<article class="contacts col-sm-6">
 					<h1>Аренда площадей и размещение</h1>
 					<h4><a href="tel:+380673794934"><span class="glyphicon glyphicon-phone"></span> 067 379 4934</a> - Светлана</h4>
 				</article>
-				<article class="col-sm-5 col-sm-offset-1">
+				<article class="contacts col-sm-5 col-sm-offset-1">
 					<h1>Размещение рекламы</h1>
 					<h4><a href="tel:+380967870076"><span class="glyphicon glyphicon-phone"></span> 096 787 0076</a> - Ирина</h4>
 				</article>
-				<article class="col-sm-6">
+				<article class="contacts col-sm-6">
 					<h1>Контент-менеджер веб ресурса</h1>
 					<h4><a href="tel:+380673794934"><span class="glyphicon glyphicon-phone"></span> 067 379 4934</a> - Анна</h4>
 				</article>
-				<article class="col-sm-5 col-sm-offset-1">
+				<article class="contacts col-sm-5 col-sm-offset-1">
 					<h1>Тех.обеспечение сайта</h1>
 					<h4><a href="tel:+380962609709"><span class="glyphicon glyphicon-phone"></span> 096 260 9709</a> - Дмитрий</h4>
 				</article>
 			</div>
 			<div class="row">
-				<article class="col-sm-12" style="text-align:center">
+				<article class="contacts col-sm-12" style="text-align:center">
 					<h1>Торговый комплекс «Плаза 3» и  «Плаза 4», Кривой Рог, проспект Гагарина, 4А</h1>
 
   					@include('layouts.map')
